84af52abdb9b51bd3ab4c2a43a49f966458a2f4c
joseomarrb <omarrosadoj@gmail.com>
2024-10-29 00:23:23 -0400

    Inicializazion del proyecto completada (html, css, js)

---
+++ script.js
@@ -0,0 +1,12 @@
+//Variables
+const userInput = document.querySelector("#user-input");
+const checkBtn = document.querySelector("#check-btn");
+const clearBtn = document.querySelector("#clear-btn");
+const results = document.querySelector("#results-div");
+
+
+//Eventos
+
+
+
+//Funciones
